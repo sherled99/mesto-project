d7c6357e1c709d50538b9a20345d3e749d2b3859
Egor Povarnitsyn <sherled99@yandex.ru>
2022-12-04 00:23:14 +0400

    Добавил веб пак

---
+++ index.js
@@ -0,0 +1,6 @@
+import './pages/index.css';
+import {enableValidation} from './scripts/components/validate.js'
+import {setStandartCards} from './scripts/components/card.js'
+
+setStandartCards();
+enableValidation();
--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-
-import {enableValidation} from './components/validate.js'
-import {setStandartCards} from './components/card.js'
-
-setStandartCards();
-enableValidation();
